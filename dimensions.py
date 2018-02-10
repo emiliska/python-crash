2a5adbcb27e91c36dfd5d38d18c8ef3cd550478c
emiliska <33560517+emiliska@users.noreply.github.com>
2018-02-09 20:56:59 -0500

    day1b

---
+++ dimensions.py
@@ -0,0 +1,9 @@
+dimensions = (200, 50)
+print("Original dimensions:")
+for dimension in dimensions:
+	print(dimension)
+# you can't modify a tuple, you can assign new value to var holding tuple
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+	print(dimension)
