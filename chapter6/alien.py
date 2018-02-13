5dad8612822f687019af564bd5fabcd9ad91717f
emiliska <33560517+emiliska@users.noreply.github.com>
2018-02-13 01:17:08 -0500

    day2

--- chapter6/alien.py
+++ chapter6/alien.py
@@ -16,17 +16,40 @@
 #print(alien_0)
 
 # undefined dictionary alien_0
-alien_0 = {}
+#alien_0 = {}
 # populate alien_0 dictionary afer initialization ^
-alien_0['color'] = 'green'
-alien_0['points'] = 5
+#alien_0['color'] = 'green'
+#alien_0['points'] = 5
 
-print(alien_0)
+#print(alien_0)
 
 # modifying values in a dictionary
-print("The alien is " + alien_0['color'] + ".")
+#print("The alien is " + alien_0['color'] + ".")
 
-alien_0['color'] = 'yellow'
-print("The alien is now " + alien_0['color'] + ".")
+#alien_0['color'] = 'yellow'
+#print("The alien is now " + alien_0['color'] + ".")
 
+# NESTING
 
+#alien_0 = {'color': 'green', 'points':5}
+#alien_1 = {'color': 'yellow', 'points':10}
+#alien_2 = {'color': 'red', 'points':15}
+
+#aliens = [alien_0, alien_1, alien_2]
+
+#for alien in aliens:
+#	print(alien)
+	
+aliens = []
+
+for alien_number in range(30):
+	new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+	aliens.append(new_alien)
+	
+# show first 5 aliens
+for alien in aliens[:5]:
+	print(alien)
+print("...")
+
+# show total created aliens
+print("Total number of aliens: " + str(len(aliens)))
--- chapter6/favorite_languages2.py
+++ chapter6/favorite_languages2.py
@@ -8,14 +8,12 @@ favorite_languages = {
 
 friends = ['emily', 'kent', 'april', 'brittany', 'michael', 'jessica', 'alex']
 
-for name in favorite_languages.keys():
-	print(name.title())
-
+# loop through each element in list friends
 for name in friends:
+# if an element is not a key in dictionary favorite_languages
 	if name not in favorite_languages.keys():
 		print(name.title() + " please take our poll!")
 	else:
 		print(name.title() +"'s favorite language is "
-			+ favorite_languages.values()
-			+ ".")
-
+				+ favorite_languages[name].title()
+				+ ".")
